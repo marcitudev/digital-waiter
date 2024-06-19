9e39f2f5532154e9e86ec923f62d418f6e4a9dc2
Márcio Costa <mncosta99@gmail.com>
2024-06-19 20:49:56 -0300

    chore: treat database port environment variable as integer

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -8,7 +8,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password:  process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: parseInt(process.env.DB_PORT as string, 10) || 5432
 });
 
 export default pool;
