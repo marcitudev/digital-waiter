514783228cc4f43ced0a7105d8debc79f1d2c41a
Márcio Costa <mncosta99@gmail.com>
2024-06-19 21:36:58 -0300

    chore: call migration service when the server runs

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import * as migrations from './migrations/execute-migrations';
 
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log('---- Server is running on port 3000 ----')
+    console.log('---- Server is running on port 3000 ----');
+    migrations.execute();
 });
