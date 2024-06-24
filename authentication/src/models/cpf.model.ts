b61baefc55616b808d1fc7af68924d55a16b9804
Márcio Costa <mncosta99@gmail.com>
2024-06-24 20:59:54 -0300

    feat: create cpf model

---
+++ authentication/src/models/cpf.model.ts
@@ -0,0 +1,37 @@
+import { ErrorEnum } from "../enums/error.enum";
+
+export class CPF{
+    cpf: string;
+
+    constructor(cpf: string){
+        if(!this.isValidCPF(cpf)){
+            throw new Error(ErrorEnum.INVALID_CPF);
+        }
+
+        this.cpf = cpf.replace(/\D/g, '');
+    }
+    
+    private isValidCPF(cpf: string): boolean{
+        const cpfWithoutMask: string = cpf.replace(/\D/g, '');
+
+        if(cpfWithoutMask.length !== 11) return false;
+        if(/^(\d)\1*$/.test(cpfWithoutMask)) return false;
+
+        const root = cpfWithoutMask.substring(0, 9);
+
+        const firstDigit = this.calcDigit(root.split(''));
+        const secondDigit = this.calcDigit(root.split('').concat(firstDigit.toString()));
+
+        const validCPF = root.concat(firstDigit.toString(), secondDigit.toString());
+        return cpfWithoutMask === validCPF;
+    }
+
+    private calcDigit(digits: string[]){
+        const factor = digits.length + 1;
+        const sum = digits.reduce((acc, curr, index) => (parseInt(curr) * (factor - index)) + acc, 0);
+
+        const remainder = sum % 11;
+
+        return remainder < 2 ? 0 : 11 - remainder;
+    }
+}
