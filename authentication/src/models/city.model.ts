faf95eaa5e867a1eb1f33108dd4851cb8ff143ac
Márcio Costa <mncosta99@gmail.com>
2024-06-24 20:58:41 -0300

    feat: create city model

---
+++ authentication/src/models/city.model.ts
@@ -0,0 +1,17 @@
+import { State } from "./state.model";
+
+export class City{
+    id: number;
+    name: string;
+    state: State;
+
+    constructor(
+        id: number,
+        name: string,
+        state: State,
+    ){
+        this.id = id;
+        this.name = name;
+        this.state = state;
+    }
+}
