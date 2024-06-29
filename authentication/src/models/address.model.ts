9e629b699617a2f477eb42ebf136226c956edeae
Márcio Costa <mncosta99@gmail.com>
2024-06-28 22:16:16 -0300

    feat: add id in address model

--- authentication/src/models/address.model.ts
+++ authentication/src/models/address.model.ts
@@ -1,14 +1,14 @@
 import { City } from "./city.model";
 
 export class Address{
-    id: number;
+    id: number | null;
     road: string;
     neighbourhood: string;
     city: City;
     number: number | null;
 
     constructor(
-        id: number,
+        id: number | null,
         road: string,
         neighbourhood: string,
         city: City,
