17df96e48244c1d383a5204450bb3d5775c4a053
Márcio Costa <mncosta99@gmail.com>
2024-06-28 22:20:47 -0300

    feat: implement create in address repository

--- authentication/src/repositories/address.repository.ts
+++ authentication/src/repositories/address.repository.ts
@@ -2,13 +2,42 @@ import { QueryResultRow } from 'pg';
 import { Address } from '../models/address.model';
 import { GenericRepository } from './generic.repository';
 import cityRepository from './city.repository';
+import pool from '../config/db';
+import { ErrorEnum } from '../enums/error.enum';
 
 class AddressRepository extends GenericRepository<Address>{
 
-    table: string = 'adresses';
+    constructor(){
+        super('adresses');
+    }
 
     async getById(id: number): Promise<Address | null>{
         return this.getByAttributeEqualTo('id', id.toString(), this.buildAddress);
+    }
+
+    async create(address: Address): Promise<Address>{
+        return new Promise<Address>((resolve, reject) => {
+            const { road, neighbourhood, city, number } = address;
+            const query = `
+                INSERT INTO adresses(
+                    road, 
+                    neighborhood, 
+                    city_id, 
+                    number
+                )
+                VALUES(
+                    LTRIM(RTRIM('${ road }')), 
+                    LTRIM(RTRIM('${ neighbourhood }')), 
+                    ${ city.id }, 
+                    ${ number ? number : 'NULL' }
+                )
+                RETURNING *
+            `
+            pool.query(query, (error, response) => {
+                if(error) reject(ErrorEnum.INVALID_ADDRESS);
+                if(response) resolve(this.buildAddress(response.rows[0]));
+            });
+        });
     }
 
     private async buildAddress(address: QueryResultRow): Promise<Address>{
