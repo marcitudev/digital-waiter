ef0d3e20a69460592896f3aa059f24f0a1875b5f
Márcio Costa <mncosta99@gmail.com>
2024-07-02 23:13:39 -0300

    fix: change column name from neighborhood to neighbourhood

--- backend/src/migrations/scripts/V003__CREATE_ADRESSES_TABLE.sql
+++ backend/src/migrations/scripts/V003__CREATE_ADRESSES_TABLE.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS adresses(
 	id SERIAL PRIMARY KEY,
 	road VARCHAR(255) NOT NULL CHECK(LENGTH(road) >= 3 AND LENGTH(road) <= 255),
-	neighborhood VARCHAR(255) NOT NULL CHECK(LENGTH(neighborhood) >= 3 AND LENGTH(neighborhood) <= 255),
+	neighbourhood VARCHAR(255) NOT NULL CHECK(LENGTH(neighbourhood) >= 3 AND LENGTH(neighbourhood) <= 255),
 	city_id INT NOT NULL,
 	number INT
 )
