3dfbdc0e5bc23221bddb0b0001f90ee8d7f81eab
Márcio Costa <mncosta99@gmail.com>
2024-07-03 21:36:16 -0300

    feat: change string query to query params and create get all request in generic repository

--- authentication/src/repositories/generic.repository.ts
+++ authentication/src/repositories/generic.repository.ts
@@ -12,11 +12,21 @@ export abstract class GenericRepository<T>{
         this.query =  `SELECT * FROM ${this.table}`;
     }
 
-    async getByAttributeEqualTo(attr: string, attrValue: string, buildObjectFunction: (row: QueryResultRow) => T | Promise<T>): Promise<T | null>{
+    protected async getAll(buildObjectFunction: (row: QueryResultRow) => Array<T> | Promise<Array<T>>){
         return new Promise((resolve, reject) => {
-            const query = this.query.concat(` WHERE ${attr} = ${attrValue}`);
+            pool.query(this.query, (error, response) => {
+                if(error) reject(error);
+                else if(response.rows.length > 0) resolve(buildObjectFunction(response.rows));
+                else resolve(null);
+            });
+        });
+    }
 
-            pool.query(query, (error, response) => {
+    protected async getByAttributeEqualTo(attr: unknown, attrValue: unknown, buildObjectFunction: (row: QueryResultRow) => T | Promise<T>): Promise<T | null>{
+        return new Promise((resolve, reject) => {
+            const query = this.query.concat(` WHERE ${attr} = $1`);
+
+            pool.query(query, [attrValue], (error, response) => {
                 if(error) reject(error);
                 else if(response.rows.length > 0) resolve(buildObjectFunction(response.rows[0]));
                 else resolve(null);
@@ -24,11 +34,11 @@ export abstract class GenericRepository<T>{
         });
     }
 
-    async getByAttributeLikeAndIgnoreCase(attr: string, attrValue: string, buildObjectFunction: (row: QueryResultRow) => T | Promise<T>): Promise<T | null>{
+    protected async getByAttributeLikeAndIgnoreCase(attr: unknown, attrValue: unknown, buildObjectFunction: (row: QueryResultRow) => T | Promise<T>): Promise<T | null>{
         return new Promise((resolve, reject) => {
-            const query = `${this.query} WHERE LOWER(${attr}) LIKE CONCAT('%', ${attrValue}, '%')`;
+            const query = `${this.query} WHERE LOWER(${attr}) LIKE CONCAT('%', $1, '%')`;
 
-            pool.query(query, (error, response) => {
+            pool.query(query, [attrValue], (error, response) => {
                 if(error) reject(error);
                 else if(response.rows.length > 0) resolve(buildObjectFunction(response.rows[0]));
                 else resolve(null);
@@ -36,22 +46,41 @@ export abstract class GenericRepository<T>{
         });
     }
 
-    async existsByAttributeEqualTo(attr: string, attrValue: string): Promise<boolean>{
+    protected async existsByAttributeEqualTo(attr: unknown, attrValue: unknown): Promise<boolean>{
         return new Promise((resolve, reject) => {
-            const query = `SELECT 1 FROM ${this.table} WHERE ${attr} = ${attrValue}`;
+            const query = `SELECT 1 FROM ${this.table} WHERE ${attr} = $1`;
 
-            pool.query(query, (error, response) => {
+            pool.query(query, [attrValue], (error, response) => {
                 if(error) reject(error);
-                resolve(response.rows[0] === 1);
+                resolve(response.rowCount! > 0);
             });
         });
     }
 
-    async existsByAttributeLikeAndIgnoreCase(attr: string, attrValue: string): Promise<boolean>{
+    protected async existsByAttributesEqualTo(attrMap: Map<unknown, unknown>): Promise<boolean>{
         return new Promise((resolve, reject) => {
-            const query = `SELECT 1 FROM ${this.table} WHERE LOWER(${attr}) LIKE CONCAT('%', ${attrValue}, '%')`;
+            let query = `SELECT 1 FROM ${this.table} WHERE `;
 
-            pool.query(query, (error, response) => {
+            let paramIndex = 1;
+            for(const [attr] of attrMap){
+                if(paramIndex > 1) 
+                    query = query.concat(' AND ');
+                query = query.concat(` ${attr} = $${paramIndex}`);
+                paramIndex++;
+            }
+
+            pool.query(query, [...attrMap.values()], (error, response) => {
+                if(error) reject(error);
+                resolve(response.rowCount! > 0);
+            });
+        });
+    }
+
+    protected async existsByAttributeLikeAndIgnoreCase(attr: unknown, attrValue: unknown): Promise<boolean>{
+        return new Promise((resolve, reject) => {
+            const query = `SELECT 1 FROM ${this.table} WHERE LOWER(${attr}) LIKE CONCAT('%', $1, '%')`;
+
+            pool.query(query, [attrValue], (error, response) => {
                 if(error) reject(error);
                 resolve(response.rows[0] === 1);
             });
