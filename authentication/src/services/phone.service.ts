661f4bddecdacc85a913c9f97bbc5870625199d9
Márcio Costa <mncosta99@gmail.com>
2024-07-03 21:41:19 -0300

    feat: add already exists validation to phone register

--- authentication/src/services/phone.service.ts
+++ authentication/src/services/phone.service.ts
@@ -1,3 +1,6 @@
+import { ErrorEnum } from '../enums/error.enum';
+import { StatusCode } from '../enums/status-code.enum';
+import { ValidationException } from '../exceptions/validation.exception';
 import { Phone } from '../models/phone.model';
 import phoneRepository from '../repositories/phone.repository';
 
@@ -6,6 +9,9 @@ class PhoneService{
     async create(phone: Phone): Promise<Phone>{
         const ddd = phone.number.substring(0,2);
         const number = phone.number.substring(2);
+
+        const existsByDDDAndNumber = await phoneRepository.existsByDDDAndPhoneNumber(ddd, number);
+        if(existsByDDDAndNumber) throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum.PHONE_NUMBER_ALREADY_EXISTS, 'Phone number already exists');
 
         return phoneRepository.create(ddd, number);
     }
