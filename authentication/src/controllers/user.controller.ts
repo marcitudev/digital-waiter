85bf2b47db1f7ff8dc267d0850332ef8af9470bb
Márcio Costa <mncosta99@gmail.com>
2024-07-03 21:40:23 -0300

    feat: add instance of user in user controller

--- authentication/src/controllers/user.controller.ts
+++ authentication/src/controllers/user.controller.ts
@@ -3,13 +3,21 @@ import { Request, Response } from 'express';
 
 // services
 import userService from '../services/user.service';
+
+// utils
 import { controllerExceptionHandler } from '../utils/exception-handler';
+import { Status } from '../enums/status.enum';
+import { User } from '../models/user.model';
+import { Phone } from '../models/phone.model';
 
 const route = express.Router();
 
-route.post('/create', async (req: Request, res: Response) => {
+route.post('/', async (req: Request, res: Response) => {
     try{
-        const user = await userService.create(req.body);
+        const { firstName, lastName, cpf, email, phone, address, password } = req.body;
+        const phoneEntity = new Phone(null, phone);
+
+        const user = await userService.create(new User(null, firstName, lastName, cpf, email, phoneEntity, address, Status.ACTIVE, password));
         return res.json(user);
     } catch(error){
         controllerExceptionHandler(req, res, error);
