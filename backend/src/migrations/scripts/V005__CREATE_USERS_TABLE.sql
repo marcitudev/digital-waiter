a1c4dc61b84a8ae95f873689fd704ce6a6a55484
Márcio Costa <mncosta99@gmail.com>
2024-07-03 21:43:06 -0300

    feat: change status to int in users table

--- backend/src/migrations/scripts/V005__CREATE_USERS_TABLE.sql
+++ backend/src/migrations/scripts/V005__CREATE_USERS_TABLE.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE IF NOT EXISTS users(
 	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(50) NOT NULL CHECK(LENGTH(first_name) >= 3 AND LENGTH(first_name) <= 50),
@@ -6,7 +8,7 @@ CREATE TABLE IF NOT EXISTS users(
 	email VARCHAR(255) NOT NULL UNIQUE CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
 	address_id INT NOT NULL,
 	phone_number_id INT UNIQUE NOT NULL,
-	status BOOLEAN NOT NULL DEFAULT 1,
+	status INT NOT NULL CHECK(status = 0 OR status = 1) DEFAULT 1,
 	password BYTEA NOT NULL,
 	CONSTRAINT fk__address_id__adresses FOREIGN KEY (address_id) REFERENCES adresses(id),
 	CONSTRAINT fk__phone_number_id__phone_numbers FOREIGN KEY (phone_number_id) REFERENCES phone_numbers(id)
