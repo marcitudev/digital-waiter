fe056c43d65455543ea6a76f68757231e6a9067f
Márcio Costa <mncosta99@gmail.com>
2024-07-08 22:45:20 -0300

    feat: add typeof validation in length decorator

--- authentication/src/decorators/length.decorator.ts
+++ authentication/src/decorators/length.decorator.ts
@@ -2,16 +2,20 @@ import { ErrorEnum } from "../enums/error.enum";
 import { StatusCode } from "../enums/status-code.enum";
 import { ValidationException } from "../exceptions/validation.exception";
 
-function Length(min: number, max: number, msg?: string){
+function Length(min: number, max: number, msg?: string): PropertyDecorator{
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return function(target: any, propertyKey: string){
-        let value: string = target[propertyKey];
+    return function(target: any, propertyKey: string | symbol){
+        let value: string = (target as { [key: string]: string })[propertyKey as string];
 
         const getter = () => value;
 
         const setter = (newValue: string) => {
-            if(newValue.length < min || newValue.length > max) {
-                throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum.INVALID_LENGTH, msg || `Invalid length: ${propertyKey}`);
+            if(newValue && typeof newValue !== 'string'){
+                throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum.INVALID_FORMAT, `${ propertyKey.toString() } is not assignable to ${ typeof newValue }`);
+            }
+
+            if(newValue && (newValue.length < min || newValue.length > max)) {
+                throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum.INVALID_LENGTH, msg || `Invalid length: ${ propertyKey.toString() }`);
             }
 
             value = newValue;
