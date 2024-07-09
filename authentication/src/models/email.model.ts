0b74783c537a8b403534aadd80ebd7544250518e
Márcio Costa <mncosta99@gmail.com>
2024-07-08 22:44:26 -0300

    feat: create email decorator

--- authentication/src/models/email.model.ts
+++ authentication/src/models/email.model.ts
@@ -1,21 +1,12 @@
-import { ErrorEnum } from "../enums/error.enum";
-import { StatusCode } from "../enums/status-code.enum";
-import { ValidationException } from "../exceptions/validation.exception";
+import { EmailFormat } from "../decorators/email.decorator";
 
 export class Email{
+
+    @EmailFormat()
     email: string;
 
     constructor(email: string){
-        if(!this.isValidEmail(email)){
-            throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum.INVALID_EMAIL, 'Invalid email format');
-        }
-
         this.email = email;
-    }
-    
-    private isValidEmail(email: string): boolean{
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
     }
 
     get value(): string{
