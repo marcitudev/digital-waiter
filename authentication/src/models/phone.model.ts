9afa3b7bccc494d76c02cd337c4dd91461f5c559
Márcio Costa <mncosta99@gmail.com>
2024-07-11 21:49:47 -0300

    feat: add phone number decorator to Phone model

--- authentication/src/models/phone.model.ts
+++ authentication/src/models/phone.model.ts
@@ -1,53 +1,14 @@
-import { ErrorEnum } from "../enums/error.enum";
-import { StatusCode } from "../enums/status-code.enum";
-import { ValidationException } from "../exceptions/validation.exception";
+import { PhoneNumberFormat } from "../decorators/phone-number.decorator";
 
 export class Phone{
     id: number | null;
+
+    @PhoneNumberFormat()
     number: string;
 
     constructor(id: number | null, number: string) {
-        if(!this.isValidPhoneNumber(number)) {
-            throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum.INVALID_PHONE_NUMBER, 'Invalid phone number format');
-        }
-
         this.id = id;
         this.number = number.replace(/\D/g, '');
-    }
-
-    private isValidPhoneNumber(number: string): boolean {
-        const ddd = number.substring(0,2);
-        const phoneNumber = number.substring(2);
-
-        return this.isValidDDD(ddd) && this.isValidNumber(phoneNumber);
-    }
-
-    private isValidDDD(ddd: string){
-        const regex = /^\d{2}$/;
-        if(!regex.test(ddd)) return false;
-
-        const dddsValidos = [
-          11, 12, 13, 14, 15, 16, 17, 18, 19,
-          21, 22, 24, 27, 28,
-          31, 32, 33, 34, 35, 37, 38,
-          41, 42, 43, 44, 45, 46,
-          47, 48, 49,
-          51, 53, 54, 55,
-          61, 62, 63, 64, 65, 66, 67, 68, 69,
-          71, 73, 74, 75, 77, 79,
-          81, 82, 83, 84, 85, 86, 87, 88, 89,
-          91, 92, 93, 94, 95, 96, 97, 98, 99
-        ];
-        return dddsValidos.includes(parseInt(ddd, 10));
-    }
-
-    private isValidNumber(number: string): boolean {
-        if(number.length !== 9) throw new Error(ErrorEnum.INVALID_PHONE_NUMBER);
-        
-        const numberWithoutMask = number.replace(/\D/g, '');
-        const regex = /^9\d{8}$/;
-        if(!regex.test(numberWithoutMask)) return false;
-        return true;
     }
 
     get value(): string{
