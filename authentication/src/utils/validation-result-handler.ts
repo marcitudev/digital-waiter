997ba7bd8c1d1d9632b0c0659b111bd89302f7e6
Márcio Costa <mncosta99@gmail.com>
2024-07-15 21:33:32 -0300

    feat: create validation result handler to request validations

---
+++ authentication/src/utils/validation-result-handler.ts
@@ -0,0 +1,17 @@
+import { Request } from 'express';
+import { validationResult } from 'express-validator';
+import { ValidationException } from '../exceptions/validation.exception';
+import { StatusCode } from '../enums/status-code.enum';
+import { ErrorEnum } from '../enums/error.enum';
+
+export function validationResultHandler(req: Request){
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        const errorsList = errors.array();
+
+        const firstError = errorsList.find(({ msg }) => !!ErrorEnum[msg as keyof typeof ErrorEnum]);
+
+        throw new ValidationException(StatusCode.BAD_REQUEST, ErrorEnum[firstError?.msg as keyof typeof ErrorEnum] || 'INVALID_PARAMS', 'Invalid request params');
+    }
+}
