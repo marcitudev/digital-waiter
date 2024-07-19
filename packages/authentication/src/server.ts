c18ab31b32dcb042be64ad5ad63b54fef5b43b07
Márcio Costa <mncosta99@gmail.com>
2024-07-18 22:30:35 -0300

    feat: implement verifyToken middleware in users endpoint

--- packages/authentication/src/server.ts
+++ packages/authentication/src/server.ts
@@ -4,6 +4,9 @@ import express, { Router } from 'express';
 import userController from './controllers/user.controller';
 import authenticationController from './controllers/authentication.controller';
 
+// middlewares
+import { verifyToken } from './controllers/authentication.controller';
+
 const app = express();
 const route = Router();
 const PORT = 3001;
@@ -11,8 +14,8 @@ const PORT = 3001;
 app.use(route);
 app.use(express.json());
 
-app.use('/users', userController);
 app.use('/authentication', authenticationController);
+app.use('/users', verifyToken, userController);
 
 app.listen(PORT, () => {
     console.log(`---- Server is running on port ${PORT} ----`);
