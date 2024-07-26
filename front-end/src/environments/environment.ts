fa7ac7b4b80f0eb6821ffb04cf8bbe0e95429976
Márcio Costa <mncosta99@gmail.com>
2024-07-25 22:06:00 -0300

    chore: create Angular project environment

---
+++ front-end/src/environments/environment.ts
@@ -0,0 +1,12 @@
+const baseServiceURL = 'http://localhost';
+
+const environment = {
+  servicesURL: {
+    main: `${baseServiceURL}:3000`,
+    authentication: `${baseServiceURL}:3001/`,
+    restaurant: `${baseServiceURL}:3002/`
+  },
+  production: false
+}
+
+export default environment;
