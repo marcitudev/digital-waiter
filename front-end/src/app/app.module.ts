3917514bdb1a4dc83dfbcc4a3dd3ad746f9f2c53
Márcio Costa <mncosta99@gmail.com>
2024-08-13 21:01:06 -0300

    chore: organize imports from app.module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,18 +1,23 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { provideHttpClient, HttpClientModule, withFetch, withInterceptors } from '@angular/common/http';
-
+import { provideHttpClient, HttpClientModule, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
+// Environment
+import environment from '../environments/environment';
+
+// Fontawesome
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 // NGRX Store
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
-import environment from '../environments/environment';
 import { appReducers } from './store/app.reducers';
 
 @NgModule({
